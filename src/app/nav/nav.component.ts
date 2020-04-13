018012a046301e1d6e5f01a5497d8f994abb99dd
ebocanegra <bocanegraedu0@gmail.com>
2020-04-13 23:17:28 +0200

    Boton para ir al inico de la página

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-nav',
@@ -10,6 +11,21 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(document).ready(function(){ //Hacia arriba
+      irArriba();
+    });
+    
+    function irArriba(){
+      $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+      });
+      $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+    }
+
   }
+
+  
 
 }
